7ca3799ed99e95ee13d4e0e80a15ba1abcc62d62
Rhyslan <79964831+Ozboy32@users.noreply.github.com>
2023-08-28 14:54:19 +1000

    Update GameEntry.java

--- Unit_4_Outcome_1/src/main/java/GameEntry.java
+++ Unit_4_Outcome_1/src/main/java/GameEntry.java
@@ -24,10 +24,6 @@ import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import org.w3c.dom.Node;
 
-/**
- *
- * @author dylan
- */
 public class GameEntry extends javax.swing.JToggleButton {
 
     /**
