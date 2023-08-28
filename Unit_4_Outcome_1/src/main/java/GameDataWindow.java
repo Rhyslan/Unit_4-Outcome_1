fa326ee0c4059024eeee018962d83d8fd5c38dd3
Rhyslan <79964831+Ozboy32@users.noreply.github.com>
2023-08-28 14:54:06 +1000

    Update GameDataWindow.java

--- Unit_4_Outcome_1/src/main/java/GameDataWindow.java
+++ Unit_4_Outcome_1/src/main/java/GameDataWindow.java
@@ -29,11 +29,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-
-/**
- *
- * @author dylan
- */
+\
 public class GameDataWindow extends javax.swing.JFrame {
 
     /**
